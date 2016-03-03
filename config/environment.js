b8a6af3a113c04e34cd7119c14dae55fa4b443af
atddev <ataldalooj@unomaha.edu>
2016-03-02 18:01:38 -0600

    metawear hybrid app step 6

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'metawear',
     environment: environment,
     baseURL: '/',
-    defaultLocationType: 'auto',
+    defaultLocationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
